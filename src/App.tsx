e686c56ed86a42d7f4df19547861eace6c872e4e
Davi Barrionuevo <davi.barrionuevo1504@gmail.com>
2026-05-18 21:02:04 -0300

    fix dates

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import { SearchLocation } from './components/SearchLocation'
 import { Footer } from './components/Footer'
 
 const forecast = [
-  { day: 'Sáb', date: '17/05', condition: 'Chuvoso', maxTemp: 20, minTemp: 18, isToday: true },
-  { day: 'Dom', date: '18/05', condition: 'Chuvoso', maxTemp: 22, minTemp: 16 },
-  { day: 'Seg', date: '19/05', condition: 'Nublado', maxTemp: 21, minTemp: 15 },
-  { day: 'Ter', date: '20/05', condition: 'Nublado', maxTemp: 20, minTemp: 14 },
-  { day: 'Qua', date: '21/05', condition: 'Nublado', maxTemp: 19, minTemp: 13 },
-  { day: 'Qui', date: '22/05', condition: 'Chuvoso', maxTemp: 17, minTemp: 12 },
-  { day: 'Sex', date: '23/05', condition: 'Ensolarado', maxTemp: 21, minTemp: 9 },
+  { day: 'Dom', date: '17/05', condition: 'Chuvoso', maxTemp: 20, minTemp: 18, isToday: true },
+  { day: 'Seg', date: '18/05', condition: 'Chuvoso', maxTemp: 22, minTemp: 16 },
+  { day: 'Ter', date: '19/05', condition: 'Nublado', maxTemp: 21, minTemp: 15 },
+  { day: 'Qua', date: '20/05', condition: 'Nublado', maxTemp: 20, minTemp: 14 },
+  { day: 'Qui', date: '21/05', condition: 'Nublado', maxTemp: 19, minTemp: 13 },
+  { day: 'Sex', date: '22/05', condition: 'Chuvoso', maxTemp: 17, minTemp: 12 },
+  { day: 'Sab', date: '23/05', condition: 'Ensolarado', maxTemp: 21, minTemp: 9 },
 ]
 
 function App() {
